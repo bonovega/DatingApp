6b23b1bdf9278edb3d70b74d8301ff9a40b3d8d5
Boris <bonovega@yahoo.com>
2021-11-01 17:18:26 +0100

    End of section 9

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,19 @@
+import { Photo } from "./photo";
+
+export interface Member {
+    id: number;
+    username: string;
+    photoUrl: string;
+    knownAs: string;
+    age: number;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    intersts: string;
+    city: string;
+    country: string;
+    photos: Photo[];
+}
+
